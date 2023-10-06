d342822077a439b2b27aebe772bb79e8b75750ac
amesp73 <amitrpatel502@gmail.com>
2023-10-06 16:26:34 -0500

    enum studio

--- src/main/java/org/launchcode/enumerableplanets/controllers/PlanetController.java
+++ src/main/java/org/launchcode/enumerableplanets/controllers/PlanetController.java
@@ -1,13 +1,16 @@
 package org.launchcode.enumerableplanets.controllers;
 
+import org.launchcode.enumerableplanets.data.Planets;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class PlanetController {
 
     @GetMapping
-    public String displayIndex() {
+    public String displayIndex(Model model) {
+        model.addAttribute("planets", Planets.values());
         return "index";
     }
 }
